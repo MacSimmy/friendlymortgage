706ac8bf746ca7f0627acbae1495db242bd74151
riverniall <riverniall@1906ce1e-c2c0-11dd-ba5c-d3b7c2b3b534>
2008-12-05 13:04:25 +0000

    Added a Main class for entry point in to the system.

---
+++ trunk/FriendlySocietyMortgageSystem/src/uk/co/exahertz/friendlysociety/Main.java
@@ -0,0 +1,22 @@
+package uk.co.exahertz.friendlysociety;
+
+/**
+ * The main class of the application, which includes the main entry point in
+ * to the Friendly Society Mortgage application.
+ * 
+ * @author Niall Scott
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public final class Main {
+    
+    /**
+     * The main entry point in to the application
+     * 
+     * @param args The command line arguments
+     * @since 0.0.1
+     */
+    public static void main(String[] args) {
+        
+    }
+}
