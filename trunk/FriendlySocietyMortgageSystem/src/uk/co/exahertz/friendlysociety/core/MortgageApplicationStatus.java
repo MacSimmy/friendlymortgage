41720f3181ca29de5d360f2761e3bcd578206463
riverniall <riverniall@1906ce1e-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-02-20 12:26:03 +0000

    Enumerated MethodOfPayment.java and added MortgageApplicationStatus enumeration.

---
+++ trunk/FriendlySocietyMortgageSystem/src/uk/co/exahertz/friendlysociety/core/MortgageApplicationStatus.java
@@ -0,0 +1,19 @@
+package uk.co.exahertz.friendlysociety.core;
+
+/**
+ * The enumeration for mortgage application status
+ * 
+ * @author Niall Scott
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+public enum MortgageApplicationStatus {
+    /** Mortgage has been offered */
+    OFFERED,
+    /** Mortgage has been denied */
+    DENIED,
+    /** Mortgage has been accepted */
+    ACCEPTED,
+    /** Mortgage is under review */
+    UNDERREVIEW;
+}
