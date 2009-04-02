bdff321b440e3150a161f6fd2e284f69cb6882fb
9uillaume <9uillaume@1906ce1e-c2c0-11dd-ba5c-d3b7c2b3b534>
2009-04-02 15:44:58 +0000

    Implemented ViewCustomerCreditChecks. Still need to do ViewCustomerCreditChecks (will be done in 2 hours)

--- trunk/FriendlySocietyMortgageSystem/src/uk/co/exahertz/friendlysociety/gui/ViewCustomerCreditChecks.java
+++ trunk/FriendlySocietyMortgageSystem/src/uk/co/exahertz/friendlysociety/gui/ViewCustomerCreditChecks.java
@@ -1,39 +1,41 @@
 /*
  * ViewCustomerCreditChecks.java
  *
- * Created on 02 April 2009, 10:55
+ * Created on 02 April 2009, 15:05
  */
 
 package uk.co.exahertz.friendlysociety.gui;
 
 import java.awt.Toolkit;
 import java.util.Collection;
+import java.util.GregorianCalendar;
 import javax.swing.table.DefaultTableModel;
 import uk.co.exahertz.friendlysociety.core.Core;
+import uk.co.exahertz.friendlysociety.core.CreditCheck;
 import uk.co.exahertz.friendlysociety.core.Customer;
-import uk.co.exahertz.friendlysociety.core.Employment;
 
 /**
  *
  * @author  gp79
  */
-public class ViewCustomerEmployments extends javax.swing.JFrame {
+public class ViewCustomerCreditChecks extends javax.swing.JFrame {
 
     private Core core;
     private DefaultTableModel myModel;
-    private Employment[] employments;
     private Customer customer;
+    private CreditCheck[] creditChecks;
     
     
     /** Creates new form ViewCustomerCreditChecks */
-    public ViewCustomerEmployments(Core core, Collection<Employment> employments, Customer customer) {
+    public ViewCustomerCreditChecks( Core core, Collection<CreditCheck> creditChecks,
+            Customer customer) {
         initComponents();
         
         if (core == null) {
           throw new IllegalArgumentException("The core instance " +
             "cannot be null.");
         }
-        if ( employments == null){
+        if ( creditChecks == null){
             throw new IllegalArgumentException("The employments instance " +
             "cannot be null.");
         }
@@ -42,7 +44,7 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
             "cannot be null.");
         }
         this.core = core;
-        this.employments = employments.toArray(new Employment[0]);
+        this.creditChecks = creditChecks.toArray(new CreditCheck[0]);
         this.customer = customer;
         
         setIconImage(Toolkit.getDefaultToolkit().getImage(
@@ -59,7 +61,6 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
 
         initModel();
         setRowsInModel();
-        
     }
 
     /** This method is called from within the constructor to
@@ -103,7 +104,7 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jButton1.setBackground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("View Employment Details");
+        jButton1.setText("View Credit Check Details");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -111,7 +112,7 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
         });
 
         jButton2.setBackground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("Close");
+        jButton2.setText("Cancel");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
@@ -127,7 +128,7 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
                 .addComponent(jButton1)
                 .addGap(18, 18, 18)
                 .addComponent(jButton2)
-                .addContainerGap(497, Short.MAX_VALUE))
+                .addContainerGap(479, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -142,14 +143,14 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
         jLabel15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/uk/co/exahertz/friendlysociety/gui/friendlysocietywatermarkmini.jpg"))); // NOI18N
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel6.setText("View All Employments of Customer");
+        jLabel6.setText("View All Credit Checks of Customer");
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 739, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 729, Short.MAX_VALUE)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel2Layout.createSequentialGroup()
@@ -165,15 +166,15 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
                         .addComponent(jLabel6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel15))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 226, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 266, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -192,17 +193,24 @@ public class ViewCustomerEmployments extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void initModel() {
+        myModel.addColumn("ID");
+        myModel.addColumn("Date");
+        myModel.addColumn("Score");
+        myModel.addColumn("Risk Status");
+        
+    }
+
 private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    // click on view employments details:
     int sr = jTable1.getSelectedRow();
-    if (sr >= 0){
-        EmploymentDetails empl = new EmploymentDetails(core, customer, employments[sr]);
-        empl.setVisible(true);
+    if ( sr >= 0 ){
+        ViewCreditCheck vcc = new ViewCreditCheck(core, creditChecks[sr], customer);
+        vcc.setVisible(true);
     }
 }//GEN-LAST:event_jButton1ActionPerformed
 
 private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-    dispose();
+ dispose();
 }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
@@ -228,34 +236,28 @@ private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 
-    private void initModel() {
-        myModel.addColumn("ID");
-        myModel.addColumn("EmployerName");
-        myModel.addColumn("Address");
+    private void setRowsInModel() {
+        emptyModel();
         
-        
+        //setting rows:
+        for (int i = 0; i < creditChecks.length; i++) {
+            Object[] data = new Object[4];
+            data[0] = creditChecks[i].getID();
+            GregorianCalendar cal = creditChecks[i].getCreditCheckDate();
+            String date = "" + cal.get(GregorianCalendar.DAY_OF_MONTH) + "/" +
+                    cal.get(GregorianCalendar.MONTH) + "/" +
+                    cal.get(GregorianCalendar.YEAR);
+            data[1] = date;
+            data[2] = creditChecks[i].getCreditCheckScore();
+            data[3] = creditChecks[i].getCreditCheckRiskStatus();
+            myModel.addRow(data);
+        }
     }
-    
     
     private void emptyModel() {
         while (myModel.getRowCount() != 0) {
             myModel.removeRow(0);
         }
-    }
-    
-    private void setRowsInModel(){
-        
-        emptyModel();
-        
-        //setting rows:
-        for (int i = 0; i < employments.length; i++) {
-            Object[] data = new Object[3];
-            data[0] = employments[i].getEmploymentID();
-            data[1] = employments[i].getEmployerName();
-            data[2] = employments[i].getEmployerAddressObject().toString();
-            myModel.addRow(data);
-        }
-        
     }
 
 }
